9aef1f6236a167ad69c3b32201231d3670b35348
Lee Robinson <lrobinson2011@gmail.com>
2021-07-11 21:56:10 -0500

    Call the SpaceX GraphQL API.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,121 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('https://api.spacex.land/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `{
+                launchesPast(limit: 10) {
+                    mission_name
+                    launch_date_local
+                    links {
+                        video_link
+                    }
+                }
+            }`
+      })
+    });
+
+    if (res.ok) {
+      const { data } = await res.json();
+      return {
+        props: {
+          launches: data.launchesPast
+        }
+      };
+    }
+
+    return {
+      status: res.status,
+      error: new Error(`Error fetching GraphQL data`)
+    };
+  }
+</script>
+
+<script>
+  export let launches;
+</script>
+
+<h1>SpaceX Launches</h1>
+<p>
+  This is an example <a class="link" target="_blank" href="https://svelte.dev"
+    >SvelteKit</a
+  >
+  application fetching GraphQL data from the public
+  <a class="link" target="_blank" href="https://api.spacex.land/graphql"
+    >SpaceX API</a
+  >.
+</p>
+<ul>
+  {#each launches as launch}
+    <li>
+      <a class="card-link" target="_blank" href={launch.links.video_link}>
+        <h2>{launch.mission_name}</h2>
+        <p>{new Date(launch.launch_date_local).toLocaleString()}</p>
+      </a>
+    </li>
+  {/each}
+</ul>
+<footer>
+  <p>
+    Created with <a class="link" target="_blank" href="https://svelte.dev"
+      >SvelteKit</a
+    >
+    and deployed with
+    <a class="link" target="_blank" href="https://vercel.com">▲ Vercel</a>.
+  </p>
+</footer>
+
+<style>
+  :global(body) {
+    font-family: Menlo, Consolas, Monaco, Liberation Mono, Lucida Console,
+      monospace;
+    background-color: #fafafa;
+    max-width: 650px;
+    margin: 32px auto;
+    padding: 0 16px;
+  }
+  h1 {
+    letter-spacing: -0.025em;
+  }
+  h2 {
+    font-size: 18px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin-top: 32px;
+  }
+  li {
+    border: 1px solid #eaeaea;
+    border-radius: 8px;
+    margin-bottom: 16px;
+    background-color: white;
+    transition: 0.15s box-shadow ease-in-out;
+  }
+  li:hover {
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12);
+  }
+  p {
+    color: #666;
+    font-size: 14px;
+    line-height: 1.75;
+  }
+  a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+  .card-link {
+    padding: 8px 24px;
+    display: block;
+  }
+  .link {
+    transition: 0.15s text-decoration ease-in-out;
+  }
+  .link:hover {
+    text-decoration: underline;
+    color: #0761d1;
+  }
+</style>

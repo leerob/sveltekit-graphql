0f13982c0758803ec71722c8b0441141ab113cab
Lee Robinson <lrobinson2011@gmail.com>
2021-07-23 09:58:02 -0500

    Add static route.

---
+++ src/routes/static.svelte
@@ -0,0 +1,54 @@
+<h1>Static Page</h1>
+<p>
+  This page is completely static. View source on
+  <a
+    class="link"
+    target="_blank"
+    rel="noopener"
+    href="https://github.com/leerob/sveltekit-graphql">GitHub</a
+  >.
+</p>
+<footer>
+  <p>
+    Created with <a
+      class="link"
+      target="_blank"
+      rel="noopener"
+      href="https://svelte.dev">SvelteKit</a
+    >
+    and deployed with
+    <a class="link" target="_blank" rel="noopener" href="https://vercel.com"
+      >▲ Vercel</a
+    >.
+  </p>
+</footer>
+
+<style>
+  :global(body) {
+    font-family: Menlo, Consolas, Monaco, Liberation Mono, Lucida Console,
+      monospace;
+    background-color: #fafafa;
+    max-width: 650px;
+    margin: 32px auto;
+    padding: 0 16px;
+  }
+  h1 {
+    letter-spacing: -0.025em;
+  }
+  p {
+    color: #666;
+    font-size: 14px;
+    line-height: 1.75;
+  }
+  a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+  .link {
+    transition: 0.15s text-decoration ease-in-out;
+    color: #0761d1;
+  }
+  .link:hover {
+    text-decoration: underline;
+  }
+</style>
